051f1e5673232c59284e6c17aa577ef64fe1e40d
Lucas Silva Gonçalves <lucassilva.goncalves@hotmail.com>
2023-07-01 13:04:34 -0300

    second commit

--- index.js
+++ index.js
@@ -1,45 +1,68 @@
 //https://pokeapi.co/
 
+//function para pegar o nome do pokemon digitado no input
 function getPokemonName() {
-    var pokemonName = document.getElementById("input-pokemon").value
-    console.log(pokemonName)
-    return pokemonName
+
+    let pokemonName = document.getElementById("input-pokemon").value.trim() //pega o nome do pokemon do input
+    var validCharacters = /^[a-zA-Z0-9\-]+$/ //verifica se contém apenas letras, números e hífen
+
+    if ((pokemonName != "" && validCharacters.test(pokemonName))) { //verifica se o campo não está vazio
+        return pokemonName
+    } else {
+        console.error("O nome do Pokémon contém caracteres inválidos ou está vazio")
+        return ""
+    }
 }
 
 //func consultar api
 function searchPokemonInfos() {
     let pokemonName = getPokemonName()
-    var url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            //console.log(data)
-            document.getElementById("div-img-pokemon").innerHTML = `<img src="${data.sprites.front_default}">`
-            document.getElementById("div-infos-pokemon").innerHTML =
-                `
-                <p> 
-                Name: ${data.name} <br>
-                Type: ${data.types[0].type.name}
-                </p>
-                `
-            defineBackground(data.types[0].type.name, verifySecondType(data.types[1].type.name))
-        })
+    
+    if (pokemonName !== "") {
+        var url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+        fetch(url)
+            .then(res => {
+                if (res.status === 404) {
+                    throw new Error("Pokemon não encontrado")
+                } else {
+                    return res.json()
+                }
+            })
+            .then(data => {
+                //console.log(data)
+                document.getElementById("div-img-pokemon").innerHTML = `<img src="${data.sprites.front_default}">`
+                document.getElementById("div-infos-pokemon").innerHTML =
+                    `
+                    <p> 
+                    Name: ${data.name} <br>
+                    Type: ${data.types[0].type.name}
+                    </p>
+                    `
+                defineBackground(data.types[0].type.name)
+
+                if (data.types[1] != undefined) {
+                    defineBackground(data.types[1].type.name)
+                }
+
+            })
+            .catch(error => {
+                console.error("Erro na chamada da API: ", error)
+                document.getElementById("div-img-pokemon").innerHTML = `<img src= "img/ashmorto.jpg">`
+                document.getElementById("div-infos-pokemon").innerHTML =
+                    `
+                        <p> 
+                            ERRO!<br>
+                            Verifique se o nome do pokemon inserido está escrito corretamente e tente novamente!
+                        </p>
+                        `
+
+            })
+    }
 }
 
-function defineBackground(pokemonType, pokemonType2) {
-    var divImgPokemon = document.getElementById("div-img-pokemon");
-    if (pokemonType2 != "fodase") {
-        divImgPokemon.style.backgroundImage = `url(img/${pokemonType2}.jpg)`;
-    } else {
-        divImgPokemon.style.backgroundImage = `url(img/${pokemonType}.jpg)`;
-    }
-    console.log(pokemonType, pokemonType2)
-}
-
-function verifySecondType(pokemonType2) {
-    if (pokemonType2 != null) {
-        return pokemonType2
-    } else {
-        return "fodase"
-    }
+// function para definir o background da div, baseado no tipo do pokemon
+function defineBackground(pokemonType) {
+    var divImgPokemon = document.getElementById("div-img-pokemon")
+    divImgPokemon.style.backgroundImage = `url(img/${pokemonType}.jpg)`
+    console.log(pokemonType)
 }
